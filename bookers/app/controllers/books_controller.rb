320e0ea9c8c75020bbe4fc379002ac51cd2811f3
mitsunobu1201 <87081727+mitsunobu1201@users.noreply.github.com>
2023-02-13 22:52:07 +0000

    [Add]一覧表示機能

--- bookers/app/controllers/books_controller.rb
+++ bookers/app/controllers/books_controller.rb
@@ -1,4 +1,24 @@
 class BooksController < ApplicationController
-  def top
+  
+  def index
+    @book = Book.new
+    @books = Book.all
+    
   end
+  
+  def create
+    # １.&2. データを受け取り新規登録するためのインスタンス作成
+    book = Book.new(books_params)
+    # 3. データをデータベースに保存するためのsaveメソッド実行
+    book.save
+    # 4. トップ画面へリダイレクト
+    redirect_to '/books'
+  end 
+  
+  private
+  # ストロングパラメータ
+  def books_params
+    params.require(:book).permit(:title, :body)
+  end
+  
 end
